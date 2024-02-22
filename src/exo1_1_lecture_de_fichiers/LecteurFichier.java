31aaa1011a0b6466940a7b9ab132243b80c5ab9a
QuentinTlgn <tauleigq@3il.fr>
2024-02-22 08:59:23 +0100

    Ajout LecteurFichier

---
+++ src/exo1_1_lecture_de_fichiers/LecteurFichier.java
@@ -0,0 +1,8 @@
+package exo1_1_lecture_de_fichiers;
+
+public class LecteurFichier {
+    public void lireFichier(String chemin) {
+        // Code pour lire le fichier et afficher son contenu
+        
+    }
+}
