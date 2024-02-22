433e21812c2ebb61b24e0002e1ea5bbb5175f321
QuentinTlgn <tauleigq@3il.fr>
2024-02-22 11:00:14 +0100

    a

---
+++ src/exo_4_1_Bibliotheque/CategorieLivre.java
@@ -0,0 +1,10 @@
+package exo_4_1_Bibliotheque;
+
+import java.util.List;
+
+public enum CategorieLivre {
+
+    FICTION, NONFICTION, ENFANT, LITTERATURE, POLICIER, SCIENCE, BD;
+
+    private List<Livre> liste;
+}
--- src/exo_4_1_Bibliotheque/Livre.java
+++ src/exo_4_1_Bibliotheque/Livre.java
@@ -1,5 +1,13 @@
 package exo_4_1_Bibliotheque;
 
 public class Livre {
-    
+    private String titre;
+    private String auteur;
+    private CategorieLivre categorie;
+
+    public Livre(String titre, String auteur, CategorieLivre categorie) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.categorie = categorie;
+    }
 }
