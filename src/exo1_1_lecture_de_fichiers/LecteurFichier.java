0d69bed74b3f88739ba790da21fc4322b0356ac4
QuentinTlgn <tauleigq@3il.fr>
2024-02-22 09:37:15 +0100

    Correction prof

--- src/exo1_1_lecture_de_fichiers/LecteurFichier.java
+++ src/exo1_1_lecture_de_fichiers/LecteurFichier.java
@@ -1,13 +1,9 @@
 package exo1_1_lecture_de_fichiers;
 
-public class LecteurFichier implements TraitementFichier{
+public class LecteurFichier{
 
     public void lireFichier(String chemin) {
         // Code pour lire le fichier et afficher son contenu
     }
 
-    @Override
-    public void traiter(String contenu) {
-        // TODO Auto-generated method stub
-    }
 }
--- src/exo1_1_lecture_de_fichiers/LecteurTexte.java
+++ src/exo1_1_lecture_de_fichiers/LecteurTexte.java
@@ -1,10 +1,16 @@
 package exo1_1_lecture_de_fichiers;
 
 public class LecteurTexte implements Lecteur {
+    private TraitementFichier traitement;
+
+    public LecteurTexte(TraitementFichier traitement) {
+        this.traitement = traitement;
+    }
 
     @Override
     public void lire() {
-        //Code pour lire un fichier texte
+        String contenu = "null";
+        traitement.traiter(contenu);
     }
     
     
