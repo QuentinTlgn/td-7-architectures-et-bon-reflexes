db00676685d9eb7bce68e45613b03566606c3cc6
QuentinTlgn <tauleigq@3il.fr>
2024-02-22 09:16:12 +0100

    Reduction de couplage

--- src/exo1_1_lecture_de_fichiers/LecteurFichier.java
+++ src/exo1_1_lecture_de_fichiers/LecteurFichier.java
@@ -3,6 +3,5 @@ package exo1_1_lecture_de_fichiers;
 public class LecteurFichier {
     public void lireFichier(String chemin) {
         // Code pour lire le fichier et afficher son contenu
-        
     }
 }
---
+++ src/exo1_1_lecture_de_fichiers/LecteurTexte.java
@@ -0,0 +1,12 @@
+package exo1_1_lecture_de_fichiers;
+
+public class LecteurTexte implements Lecteur {
+
+    @Override
+    public String lire() {
+        //Code pour lire un fichier texte
+        return "Texte du fichier";
+    }
+    
+    
+}
